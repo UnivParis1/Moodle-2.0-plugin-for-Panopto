947ad227f36618c65a06956402c37fd5ec4fb697
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2020-03-24 12:37:33 +0100

    add french translation

---
+++ lang/fr/block_panopto.php
@@ -0,0 +1,38 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://cours-cavej-test.univ-paris1.fr
+ *
+ * @package    block
+ * @subpackage panopto
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['completed_recordings'] = 'Enregistrements terminés';
+$string['course_settings'] = 'Paramètres du cours';
+$string['download_recorder'] = 'Télécharger l\'application Panopto Recorder';
+$string['links'] = 'Liens';
+$string['live_sessions'] = 'Session en direct';
+$string['no_live_sessions'] = 'Pas d\'évènement en direct';
+$string['pluginname'] = 'Amphi virtuel Panopto';
+$string['show_all'] = 'Afficher tout';
+$string['show_less'] = 'Réduire';
+$string['take_notes'] = 'Prendre des notes';
+$string['watch_live'] = 'Regarder le direct';
